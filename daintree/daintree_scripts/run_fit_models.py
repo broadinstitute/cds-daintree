2e5602ed4853dbc363c2d4c8b596be73f2e1aa95
Nayeem Aquib <101162634+naquib314@users.noreply.github.com>
2025-02-10 03:53:31 -0500

    Added a ConfigManager

--- daintree/daintree_scripts/run_fit_models.py
+++ daintree/daintree_scripts/run_fit_models.py
@@ -220,13 +220,27 @@ class DataProcessor:
             feature_name = re.sub(r'[\s-]+', '_', col) + f"_{feature_dataset_name}"
         return feature_name, feature_label, given_id
 
-    def _partition_inputs(self, dep_matrix, ensemble_config, save_pref, out_name="partitions.csv"):
+    def generate_feature_info(self, ipt_dicts):
+        """Generate feature information.
+        
+        Args:
+            ipt_dicts: Input dictionaries
+        """
+        dsets = []
+        for dset_name, dset_value in ipt_dicts["data"].items():
+            if dset_value["table_type"] == "feature":
+                dsets.append(dset_name)
+        fnames = [str(self.save_pref / (dset + ".csv")) for dset in dsets]
+
+        df = pd.DataFrame({"dataset": dsets, "filename": fnames,})
+        return df
+
+    def partition_inputs(self, dep_matrix, ensemble_config, out_name="partitions.csv"):
         """Partition inputs for parallel processing.
         
         Args:
             dep_matrix: Dependency matrix
             ensemble_config: Configuration for ensemble models
-            save_pref: Save directory path
             out_name: Output filename
         """
         num_genes = dep_matrix.shape[1]
@@ -250,69 +264,7 @@ class DataProcessor:
                 "model": np.concatenate(models),
             }
         )
-        param_df.to_csv(save_pref / out_name, index=False)
-
-    def _generate_config(self, input_dict, relation="All"):
-        """Generate configuration for model.
-        
-        Args:
-            input_dict: Input dictionary containing model configuration
-            relation: Relation type
-        """
-        model_name = input_dict["model_name"]
-        data = input_dict["data"]
-
-        features = [
-            key for key, value in data.items()
-            if value.get("table_type") == "feature"
-        ]
-        required = [
-            key for key, value in data.items()
-            if value.get("table_type") == "feature" and value.get("required", False)
-        ]
-        exempt = [
-            key for key, value in data.items()
-            if value.get("table_type") == "feature" and value.get("exempt", False)
-        ]
-        relation = next(
-            (value["relation"] for value in data.values() if value.get("table_type") == "target_matrix"),
-            "All"
-        )
-        model_config = {
-            "Features": features,
-            "Required": required,
-            "Relation": relation,
-            "Jobs": 10
-        }
-        
-        if relation == "MatchRelated":
-            related = next(
-                (key for key, value in data.items() if value.get("table_type") == "relation"),
-                None
-            )
-            if related:
-                model_config["Related"] = related
-        
-        if exempt:
-            model_config["Exempt"] = exempt
-
-        return {model_name: model_config}
-
-    def _generate_feature_info(self, ipt_dicts, save_pref):
-        """Generate feature information.
-        
-        Args:
-            ipt_dicts: Input dictionaries
-            save_pref: Save directory path
-        """
-        dsets = []
-        for dset_name, dset_value in ipt_dicts["data"].items():
-            if dset_value["table_type"] == "feature":
-                dsets.append(dset_name)
-        fnames = [str(save_pref / (dset + ".csv")) for dset in dsets]
-
-        df = pd.DataFrame({"dataset": dsets, "filename": fnames,})
-        return df
+        param_df.to_csv(self.save_pref / out_name, index=False)
 
 
 class SparklesRunner:
@@ -356,7 +308,6 @@ class SparklesRunner:
                 "--related-table", "related.ftr"
             ])
         return base_cmd
-
 
     def _watch_jobs(self):
         subprocess.check_call([self.sparkles_path, "--config", self.sparkles_config, 
@@ -447,38 +398,12 @@ class SparklesRunner:
         self._process_completed_jobs()
 
 
-class ModelFitter:
-    def __init__(self, input_files, ensemble_config, sparkles_config, 
-                 save_dir=None, test=False, skipfit=False, 
-                 upload_to_taiga=None, 
-                 restrict_targets=False, restrict_to=None,
-                 filter_columns=None):
-        self.tc = create_taiga_client_v3()
-        self.save_pref = Path(save_dir) if save_dir else Path.cwd()
-        self.input_files = input_files
-        self.ensemble_config = ensemble_config
-        self.sparkles_config = sparkles_config
-        self.test = test
-        self.skipfit = skipfit
-        self.upload_to_taiga = upload_to_taiga
-        self.restrict_targets = restrict_targets
-        self.restrict_to = restrict_to
-        self.filter_columns = filter_columns.split(",") if filter_columns else None
-        self.save_pref.mkdir(parents=True, exist_ok=True)
-        self.data_processor = DataProcessor(self.save_pref)
+class ConfigManager:
+    def __init__(self, save_pref):
+        self.save_pref = Path(save_pref)
 
-    def _check_file_locs(self, ipt, config):
-        ipt_features = list(ipt["data"].keys())
-        for model_name, model_config in config.items():
-            f_set = set(model_config.get("Features", []) + model_config.get("Required", []))
-            if model_config.get("Relation") not in ["All", "MatchTarget"]:
-                f_set.add(model_config.get("Related", ""))
-            features = list(f_set)
-            for f in features:
-                assert f in ipt_features, f"Feature {f} in model config file does not have corresponding input in {model_name}"
-
-    def _create_output_config(self, model_name, screen_name, input_config, 
-                            feature_metadata_id, ensemble_id, prediction_matrix_id):
+    def create_output_config(self, model_name, screen_name, input_config, 
+                           feature_metadata_id, ensemble_id, prediction_matrix_id):
         """Create output configuration JSON file.
         
         Args:
@@ -508,98 +433,7 @@ class ModelFitter:
         }
         return config
 
-    def _upload_results_to_taiga(self, model_name, screen_name, ipt_dict):
-        """Upload results to Taiga and create output config file.
-        
-        Args:
-            model_name: Name of the model
-            screen_name: Name of the screen
-            ipt_dict: Input configuration dictionary
-        """
-        feature_metadata_filename = f"FeatureMetadata{model_name}{screen_name}.csv"
-        ensemble_filename = f"Ensemble{model_name}{screen_name}.csv"
-        predictions_filename = f"Predictions{model_name}{screen_name}.csv"
-
-        feature_metadata_taiga_info = update_taiga(
-            self.upload_to_taiga,
-            f"Updated Feature Metadata for Model: {model_name} and Screen: {screen_name}",
-            f"FeatureMetadata{model_name}{screen_name}",
-            self.save_pref / feature_metadata_filename,
-            "csv_table",
-        )
-        print(f"Feature Metadata uploaded to Taiga: {feature_metadata_taiga_info}")
-
-        ensemble_taiga_info = update_taiga(
-            self.upload_to_taiga,
-            f"Updated Ensemble for Model: {model_name} and Screen: {screen_name}",
-            f"Ensemble{model_name}{screen_name}",
-            self.save_pref / ensemble_filename,
-            "csv_table",
-        )
-        print(f"Ensemble uploaded to Taiga: {ensemble_taiga_info}")
-
-        predictions_taiga_info = update_taiga(
-            self.upload_to_taiga,
-            f"Updated Predictions for Model: {model_name} and Screen: {screen_name}",
-            f"Predictions{model_name}{screen_name}",
-            self.save_pref / predictions_filename,
-            "csv_table",
-        )
-        print(f"Predictions uploaded to Taiga: {predictions_taiga_info}")
-
-        output_config = self._create_output_config(
-            model_name=model_name,
-            screen_name=screen_name,
-            input_config=ipt_dict,
-            feature_metadata_id=feature_metadata_taiga_info,
-            ensemble_id=ensemble_taiga_info,
-            prediction_matrix_id=predictions_taiga_info
-        )
-
-        output_config_dir = self.save_pref / "output_config_files"
-        output_config_dir.mkdir(parents=True, exist_ok=True)
-        output_config_filename = f"OutputConfig{model_name}{screen_name}.json"
-        output_config_file = output_config_dir / output_config_filename
-
-        with open(output_config_file, 'w') as f:
-            json.dump(output_config, f, indent=4)
-        print(f"Created output config file: {output_config_file}")
-
-        return feature_metadata_taiga_info, ensemble_taiga_info, predictions_taiga_info
-
-    def _partition_inputs(self, dep_matrix, ensemble_config, save_pref, out_name="partitions.csv"):
-        """Partition inputs for parallel processing.
-        
-        Args:
-            dep_matrix: Dependency matrix
-            ensemble_config: Configuration for ensemble models
-            save_pref: Save directory path
-            out_name: Output filename
-        """
-        num_genes = dep_matrix.shape[1]
-        start_indexes = []
-        end_indexes = []
-        models = []
-
-        for model_name, model_config in ensemble_config.items():
-            num_jobs = int(model_config["Jobs"])
-            start_index = np.array(range(0, num_genes, num_jobs))
-            end_index = start_index + num_jobs
-            end_index[-1] = num_genes
-            start_indexes.append(start_index)
-            end_indexes.append(end_index)
-            models.append([model_name] * len(start_index))
-
-        param_df = pd.DataFrame(
-            {
-                "start": np.concatenate(start_indexes),
-                "end": np.concatenate(end_indexes),
-                "model": np.concatenate(models),
-            }
-        )
-        param_df.to_csv(save_pref / out_name, index=False)
-
-    def _generate_config(self, input_dict, relation="All"):
+    def generate_model_config(self, input_dict, relation="All"):
         """Generate configuration for model.
         
         Args:
@@ -645,21 +479,114 @@ class ModelFitter:
 
         return {model_name: model_config}
 
-    def _generate_feature_info(self, ipt_dicts, save_pref):
-        """Generate feature information.
+    def save_config(self, config, filename):
+        """Save configuration to a file.
         
         Args:
-            ipt_dicts: Input dictionaries
-            save_pref: Save directory path
+            config: Configuration dictionary
+            filename: Name of the file to save to
         """
-        dsets = []
-        for dset_name, dset_value in ipt_dicts["data"].items():
-            if dset_value["table_type"] == "feature":
-                dsets.append(dset_name)
-        fnames = [str(save_pref / (dset + ".csv")) for dset in dsets]
+        config_path = self.save_pref / filename
+        with open(config_path, "w") as f:
+            yaml.dump(config, f, sort_keys=True)
+        return str(config_path)
 
-        df = pd.DataFrame({"dataset": dsets, "filename": fnames,})
-        return df
+    def load_config(self, config_path):
+        """Load configuration from a file.
+        
+        Args:
+            config_path: Path to the configuration file
+            
+        Returns:
+            dict: Loaded configuration
+        """
+        with open(config_path, "r") as f:
+            return yaml.load(f, yaml.SafeLoader)
+
+
+class ModelFitter:
+    def __init__(self, input_files, ensemble_config, sparkles_config, 
+                 save_dir=None, test=False, skipfit=False, 
+                 upload_to_taiga=None, 
+                 restrict_targets=False, restrict_to=None,
+                 filter_columns=None):
+        self.tc = create_taiga_client_v3()
+        self.save_pref = Path(save_dir) if save_dir else Path.cwd()
+        self.input_files = input_files
+        self.ensemble_config = ensemble_config
+        self.sparkles_config = sparkles_config
+        self.test = test
+        self.skipfit = skipfit
+        self.upload_to_taiga = upload_to_taiga
+        self.restrict_targets = restrict_targets
+        self.restrict_to = restrict_to
+        self.filter_columns = filter_columns.split(",") if filter_columns else None
+        self.save_pref.mkdir(parents=True, exist_ok=True)
+        self.data_processor = DataProcessor(self.save_pref)
+        self.config_manager = ConfigManager(self.save_pref)
+
+    def _check_file_locs(self, ipt, config):
+        ipt_features = list(ipt["data"].keys())
+        for model_name, model_config in config.items():
+            f_set = set(model_config.get("Features", []) + model_config.get("Required", []))
+            if model_config.get("Relation") not in ["All", "MatchTarget"]:
+                f_set.add(model_config.get("Related", ""))
+            features = list(f_set)
+            for f in features:
+                assert f in ipt_features, f"Feature {f} in model config file does not have corresponding input in {model_name}"
+
+    def _upload_results_to_taiga(self, model_name, screen_name, ipt_dict):
+        """Upload results to Taiga and create output config file."""
+        feature_metadata_filename = f"FeatureMetadata{model_name}{screen_name}.csv"
+        ensemble_filename = f"Ensemble{model_name}{screen_name}.csv"
+        predictions_filename = f"Predictions{model_name}{screen_name}.csv"
+
+        feature_metadata_taiga_info = update_taiga(
+            self.upload_to_taiga,
+            f"Updated Feature Metadata for Model: {model_name} and Screen: {screen_name}",
+            f"FeatureMetadata{model_name}{screen_name}",
+            self.save_pref / feature_metadata_filename,
+            "csv_table",
+        )
+        print(f"Feature Metadata uploaded to Taiga: {feature_metadata_taiga_info}")
+
+        ensemble_taiga_info = update_taiga(
+            self.upload_to_taiga,
+            f"Updated Ensemble for Model: {model_name} and Screen: {screen_name}",
+            f"Ensemble{model_name}{screen_name}",
+            self.save_pref / ensemble_filename,
+            "csv_table",
+        )
+        print(f"Ensemble uploaded to Taiga: {ensemble_taiga_info}")
+
+        predictions_taiga_info = update_taiga(
+            self.upload_to_taiga,
+            f"Updated Predictions for Model: {model_name} and Screen: {screen_name}",
+            f"Predictions{model_name}{screen_name}",
+            self.save_pref / predictions_filename,
+            "csv_table",
+        )
+        print(f"Predictions uploaded to Taiga: {predictions_taiga_info}")
+
+        output_config = self.config_manager.create_output_config(
+            model_name=model_name,
+            screen_name=screen_name,
+            input_config=ipt_dict,
+            feature_metadata_id=feature_metadata_taiga_info,
+            ensemble_id=ensemble_taiga_info,
+            prediction_matrix_id=predictions_taiga_info
+        )
+
+        output_config_dir = self.save_pref / "output_config_files"
+        output_config_dir.mkdir(parents=True, exist_ok=True)
+        output_config_filename = f"OutputConfig{model_name}{screen_name}.json"
+        output_config_file = output_config_dir / output_config_filename
+
+        with open(output_config_file, 'w') as f:
+            json.dump(output_config, f, indent=4)
+        print(f"Created output config file: {output_config_file}")
+
+        return feature_metadata_taiga_info, ensemble_taiga_info, predictions_taiga_info
 
     def run(self):
         print("loading input files...")
@@ -668,17 +595,11 @@ class ModelFitter:
 
         # Auto-generate config if not provided
         if not self.ensemble_config:
-            config = self._generate_config(ipt_dict, relation="All")
+            config = self.config_manager.generate_model_config(ipt_dict, relation="All")
             model_config_name = f"model-config_temp_{dt.datetime.now().strftime('%Y-%m-%d-%H-%M-%S-%f')}.yaml"
-            config_path = self.save_pref / model_config_name
-            
-            with open(config_path, "w") as f:
-                yaml.dump(config, f, sort_keys=True)
-            
-            self.ensemble_config = str(config_path)
+            self.ensemble_config = self.config_manager.save_config(config, model_config_name)
 
-        with open(self.ensemble_config, "r") as f:
-            config_dict = yaml.load(f, yaml.SafeLoader)
+        config_dict = self.config_manager.load_config(self.ensemble_config)
 
         print("validating inputs...")
         self._check_file_locs(ipt_dict, config_dict)
@@ -687,7 +608,7 @@ class ModelFitter:
         ), "Exactly one dataset labeled 'target_matrix' is required"
 
         print("generating feature index and files...")
-        feature_info = self._generate_feature_info(ipt_dict, self.save_pref)
+        feature_info = self.data_processor.generate_feature_info(ipt_dict)
 
         print("processing relations...")
         # determine whether to provide output_related
@@ -776,7 +697,7 @@ class ModelFitter:
         subprocess.check_call(prep_x_cmd)
 
         print("partitioning inputs...")
-        self._partition_inputs(df_dep, config_dict, self.save_pref, out_name="partitions.csv")
+        self.data_processor.partition_inputs(df_dep, config_dict)
 
         if not self.skipfit:
             print("submitting fit jobs...")
@@ -789,8 +710,7 @@ class ModelFitter:
             dt_hash = sparkles_runner.run()
             sparkles_runner.validate()
             
-            data_processor = DataProcessor(self.save_pref)
-            df_ensemble, df_predictions = data_processor.gather_ensemble_tasks(
+            df_ensemble, df_predictions = self.data_processor.gather_ensemble_tasks(
                 features=str(self.save_pref / "X.ftr"), 
                 targets=str(self.save_pref / "target_matrix.ftr"), 
                 top_n=50
@@ -807,6 +727,7 @@ class ModelFitter:
                 )
         else:
             print("skipping fitting and ending run")
+
 
 @cli.command()
 @click.option(
