2a72594a6b4cd490e4c45212b20c8e7d00aad96f
Nayeem Aquib <101162634+naquib314@users.noreply.github.com>
2025-02-11 13:30:54 -0500

    Update the readme

--- daintree/run-daintree.sh
+++ daintree/run-daintree.sh
@@ -4,8 +4,10 @@
 
 # Default values
 TARGETS=""
-TEST="True"
+TEST="True" # I am keeping it True because I have been only using this bash script for testing purposes and once conseq rules are implemented, I am not sure if this script will be used for production. 
 SKIPFIT="False"
+INPUT_CONFIG="model-map.json"
+UPLOAD_TO_TAIGA=""
 
 # Parse named parameters
 for i in "$@"; do
@@ -19,12 +21,18 @@ for i in "$@"; do
         --skipfit=*)
         SKIPFIT="${i#*=}"
         ;;
+        --input-config=*)
+        INPUT_CONFIG="${i#*=}"
+        ;;
+        --upload-to-taiga=*)
+        UPLOAD_TO_TAIGA="${i#*=}"
+        ;;
     esac
 done
 
 docker run --pull=always --rm \
   -v "${PWD}"/output_data:/daintree/output_data \
-  -v "${PWD}"/model-map.json:/daintree/model-map.json \
+  -v "${PWD}"/"${INPUT_CONFIG}":/daintree/model-map.json \
   -v "${PWD}"/sparkles-config:/daintree/sparkles-config \
   us.gcr.io/broad-achilles/daintree:v5 \
   collect-and-fit \
@@ -33,4 +41,5 @@ docker run --pull=always --rm \
   --out /daintree/output_data \
   --test "$TEST" \
   --skipfit "$SKIPFIT" \
-  $([ -n "$TARGETS" ] && echo "--restrict-targets-to $TARGETS")
+  $([ -n "$TARGETS" ] && echo "--restrict-targets-to $TARGETS") \
+  $([ -n "$UPLOAD_TO_TAIGA" ] && echo "--upload-to-taiga $UPLOAD_TO_TAIGA")
