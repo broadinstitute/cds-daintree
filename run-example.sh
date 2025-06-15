44c8f0b898d3ed710c28547853fae640efcd7692
Philip Montgomery <pmontgom@broadinstitute.org>
2025-06-15 20:47:05 +0000

    WIP

--- run-example.sh
+++ run-example.sh
@@ -4,5 +4,5 @@ DOCKER_IMAGE=us-central1-docker.pkg.dev/cds-docker-containers/docker/daintree:te
 docker build . -t $DOCKER_IMAGE
 docker push $DOCKER_IMAGE
 
-daintree-runner create-sparkles-workflow --config example/model-map.json --models-per-task 10 --test-first-n-tasks 10 > workflow.tmp
-sparkles workflow run --nodes 10 daintree-test-10 workflow.tmp -i $DOCKER_IMAGE -u/home/ubuntu/.taiga/token:.taiga-token -u example/model-map.json:model_config.json
+daintree-runner create-sparkles-workflow --config example/model-map.json --models-per-task 5 --test-first-n-tasks 20 > workflow.tmp
+sparkles workflow run --nodes 4 daintree-test-12 workflow.tmp -i $DOCKER_IMAGE -u/home/ubuntu/.taiga/token:.taiga-token -u example/model-map.json:model_config.json
