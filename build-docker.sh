5b22c153d4234285d40c964285fc857ba1e49858
Philip Montgomery <pmontgom@broadinstitute.org>
2026-04-15 20:37:08 -0400

    bumped version of daintree

--- build-docker.sh
+++ build-docker.sh
@@ -2,7 +2,7 @@ set -x
 
 VERSION=`poetry version --short`
 DOCKER_IMAGE=us-central1-docker.pkg.dev/cds-docker-containers/docker/daintree:$VERSION
-if [[ docker manifest inspect $DOCKER_IMAGE > /dev/null ]]; then
+if docker manifest inspect $DOCKER_IMAGE > /dev/null ; then
   echo "Image $DOCKER_IMAGE already exists. Did you bump the version with 'poetry version minor'?"
   echo "Not building"
   exit 1
